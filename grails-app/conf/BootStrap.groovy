f8b8c7a3e4fc82f047c4f732835ba241c5f6f835
gcampoy <guillecampoy@gmail.com>
2015-05-13 10:40:08 -0300

    Se instala SpringSecurity, se agregan usuarios para test

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,16 @@
+import proyectocumple.Rol
+import proyectocumple.Usuario
+import proyectocumple.UsuarioRol
 class BootStrap {
-
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+   def init = { servletContext ->
+      def adminRole = new Rol(authority: 'ROLE_ADMIN').save(flush: true)
+      def userRole = new Rol(authority: 'ROLE_USER').save(flush: true)
+      def testUser = new Usuario(username: 'guille', password: 'campoy')
+      def testUser2 = new Usuario(username: 'pepe', password: '123456')
+      testUser.save(flush: true)
+      testUser2.save(flush: true)
+      UsuarioRol.create testUser, adminRole, true
+      UsuarioRol.create testUser2, userRole, true
+    
+   }
 }
---
+++ grails-app/domain/proyectocumple/Rol.groovy
@@ -0,0 +1,14 @@
+package proyectocumple
+
+class Rol {
+
+	String authority
+
+	static mapping = {
+		cache true
+	}
+
+	static constraints = {
+		authority blank: false, unique: true
+	}
+}
